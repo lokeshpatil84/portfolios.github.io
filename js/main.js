fbd76298ff634459f78b664f0b77835225277b0e
lokesh patil <lokeshpatil8484@gmail.com>
2021-12-03 23:53:08 +0530

    change

--- js/main.js
+++ js/main.js
@@ -59,14 +59,6 @@ $(document).ready(function(){
             0:{
                 items: 1,
                 nav: false
-            },
-            600:{
-                items: 2,
-                nav: false
-            },
-            1000:{
-                items: 3,
-                nav: false
             }
         }
     });
